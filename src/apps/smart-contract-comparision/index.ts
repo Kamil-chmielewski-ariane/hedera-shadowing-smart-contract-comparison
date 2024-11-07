e284c13d23ff8d715d3c13032155722dbd2df5a5
Kamil Chmielewski <kamil.chmielewski@arianelabs.com>
2024-11-07 16:17:26 +0100

    added compare in index

--- src/apps/smart-contract-comparision/index.ts
+++ src/apps/smart-contract-comparision/index.ts
@@ -1,13 +1,15 @@
-import { compareSmartContractRootState } from '@/apps/smart-contract-comparision/blockchain-utils/compare-smart-contract-root-state';
-import {websocketConnection, websocketEvents} from '@/api/websocket/websocket-connection';
+import {
+	websocketConnection,
+	websocketEvents,
+} from '@/api/websocket/websocket-connection';
 import { TransactionStatusResponse } from '@/utils/types';
+import { compareSmartContractRootState } from '@/apps/smart-contract-comparision/blockchain-utils/compare-smart-contract-root-state';
 
 (async () => {
 	websocketConnection();
 	await new Promise((resolve) => setTimeout(resolve, 2000));
 
 	websocketEvents.on('websocket', (contractData: TransactionStatusResponse) => {
-		console.log(contractData);
-		// compareSmartContractRootState(contractData.transactionPayload);
+		compareSmartContractRootState(contractData);
 	});
 })();
