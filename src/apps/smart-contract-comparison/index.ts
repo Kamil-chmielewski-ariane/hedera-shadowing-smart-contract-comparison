5ae3afb8ebab801890445763b9a310af1113f734
Kamil Chmielewski <kamil.chmielewski@arianelabs.com>
2024-12-02 11:04:37 +0100

    added-transaction-checker-into-project

--- src/apps/smart-contract-comparison/index.ts
+++ src/apps/smart-contract-comparison/index.ts
@@ -4,13 +4,21 @@ import {
 } from '@/api/websocket/websocket-connection';
 import { TransactionStatusResponse } from '@/utils/types';
 import { compareSmartContractRootState } from '@/apps/smart-contract-comparison/blockchain-utils/compare-smart-contract-root-state';
+import { writeLogFile } from '@/utils/helpers/write-log-file';
+import { transactionStatusAccuracyChecker } from '@/apps/smart-contract-comparison/hedera/transaction-status-accuracy-checker';
 
 (async () => {
+	await writeLogFile(
+		`logs/transaction-checker.csv`,
+		'BlockNumber,EthereumTransactioHash,HederaTransactionHash,Result \r\n'
+	);
+
 	// Start listening for the shadowing api requests from evm_shadowing api
 	websocketConnection();
 	await new Promise((resolve) => setTimeout(resolve, 2000));
 
-	websocketEvents.on('websocket', (contractData: TransactionStatusResponse) => {
-		compareSmartContractRootState(contractData);
+	websocketEvents.on('websocket', async (contractData: TransactionStatusResponse) => {
+		await transactionStatusAccuracyChecker(contractData);
+		await compareSmartContractRootState(contractData);
 	});
 })();
