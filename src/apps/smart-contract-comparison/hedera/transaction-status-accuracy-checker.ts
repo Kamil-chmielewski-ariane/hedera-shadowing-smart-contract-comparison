cffb4033ad53abe88adf9a2d7c0cb3be03b6eaae
Kamil Chmielewski <kamil.chmielewski@arianelabs.com>
2024-12-03 14:19:48 +0100

    refactored transactionStatusAccuracyChecker function

--- src/apps/smart-contract-comparison/hedera/transaction-status-accuracy-checker.ts
+++ src/apps/smart-contract-comparison/hedera/transaction-status-accuracy-checker.ts
@@ -1,24 +1,22 @@
 import { writeLogFile } from '@/utils/helpers/write-log-file';
-import { ContractType } from '@/utils/types';
-import { getHederaTransactionById } from '@/api/hedera-mirror-node/get-hedera-transaction-by-id';
+import { TransactionStatusResponse } from '@/utils/types';
 
 export async function transactionStatusAccuracyChecker(
-	contractRootData: ContractType
+	contractRootData: TransactionStatusResponse
 ) {
 	console.log(`Checking transaction ${contractRootData.hederaTransactionHash}`);
-	const { blockNumber, hederaTransactionHash, ethereumTransactionHash } =
-		contractRootData;
+	const {
+		blockNumber,
+		hederaTransactionHash,
+		ethereumTransactionHash,
+		status,
+	} = contractRootData;
 
-	if (contractRootData && hederaTransactionHash) {
-		const data = await getHederaTransactionById(hederaTransactionHash);
-		const transactionStatus = data?.result;
-
-		if (data && transactionStatus) {
-			await writeLogFile(
-				`logs/transaction-checker.csv`,
-				`${blockNumber},${ethereumTransactionHash},${hederaTransactionHash},${transactionStatus} \r\n`
-			);
-		}
-		console.log(`Finished checking transaction - result: ${transactionStatus}`);
+	if (contractRootData) {
+		await writeLogFile(
+			`logs/transaction-checker.csv`,
+			`${blockNumber},${ethereumTransactionHash},${hederaTransactionHash},${status} \r\n`
+		);
+		console.log(`Finished checking transaction - result: ${status}`);
 	}
 }
